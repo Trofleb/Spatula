51117318627fbd015a23e083d6fd43575b475e97
Teo Stocco <teo.stocco@epfl.ch>
2015-04-18 13:41:41 +0200

    Provide get for http request

--- src/main/scala/spatulapp/Spatula.scala
+++ src/main/scala/spatulapp/Spatula.scala
@@ -3,19 +3,33 @@ package spatulapp
 import org.scalajs.dom
 import org.scalajs.jquery.jQuery
 
+import scala.io.Source
 import scala.scalajs.js
 
 object Spatula extends js.JSApp {
 
+  val $ = jQuery
+
   def main(): Unit = {
 
-    jQuery("body").append("<p>[message]</p>")
+    $("body").append("<p>[message]</p>")
 
     log("meeeeh")
+
+    get("http://google.com")(log)
 
   }
 
   def log(m: String): Unit =
     dom.console.log(m)
+
+  def get(url: String)(callback: String => Unit): Unit =
+    $.get(url, {
+      e: Any => e match {
+        case s: String => callback(s)
+        case _ => log("get: matcherror")
+      }
+    })
+
 
 }
