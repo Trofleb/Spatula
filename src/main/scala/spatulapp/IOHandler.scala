6d4301ee43c37726751e023792715273d220ebc7
tOverney <tristan.overney@epfl.ch>
2015-04-18 16:14:34 +0200

    no more spaghetti code

---
+++ src/main/scala/spatulapp/IOHandler.scala
@@ -0,0 +1,19 @@
+package spatulapp
+
+
+import org.scalajs.dom
+import org.scalajs.dom.raw._
+import org.scalajs.dom.ext._
+
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+
+object IOHandler {
+  def log(m: String): Unit =
+    dom.console.log(m)
+
+  def get(url: String): Future[XMLHttpRequest] = {
+    Ajax.get(url = "http://www.zifeo.com/hackathon.php?key=xi3nu2859323xu2&get="+url)
+  }
+}
--- src/main/scala/spatulapp/Spatula.scala
+++ src/main/scala/spatulapp/Spatula.scala
@@ -1,15 +1,14 @@
 package spatulapp
 
 
-import org.scalajs.dom
-import org.scalajs.dom.XMLHttpRequest
+import scala.scalajs.js
+import org.scalajs.dom.raw._
 import org.scalajs.jquery.jQuery
 import org.scalajs.dom.ext._
 
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
-import scala.scalajs.js
 import scala.util.{Failure, Success}
 
 object Spatula extends js.JSApp {
@@ -22,22 +21,17 @@ object Spatula extends js.JSApp {
 
     //log("meeeeh")
 
-    val request = get("http://bigoven.com")
+    val request = IOHandler.get("http://bigoven.com")
 
     request onComplete {
-      case Success(s) => println(s.responseText)
-      case Failure(_) => log("There was an error fetching the webpage")
+      case Success(s) => println(s.responseXML); IOHandler.log("hello")
+      case Failure(_) => IOHandler.log("There was an error fetching the webpage")
     }
 
 
   }
 
-  def log(m: String): Unit =
-    dom.console.log(m)
 
-  def get(url: String): Future[XMLHttpRequest] = {
-    Ajax.get(url = "http://www.zifeo.com/hackathon.php?key=xi3nu2859323xu2&get="+url)
-  }
 
 
 }
