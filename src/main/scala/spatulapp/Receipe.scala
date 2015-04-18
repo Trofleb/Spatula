5bbe58a7f7dc7bda6483f6d4eb94b8f15b0ef9c9
Trofleb <ncasademont@bluewin.ch>
2015-04-18 15:18:31 +0200

    Added Receipe class

---
+++ src/main/scala/spatulapp/Receipe.scala
@@ -0,0 +1,19 @@
+
+
+class Receipe(
+	val title: String
+	val stars: Int
+	val pictures: Seq[String]
+	val ingredients: Seq[String]
+	val stepByStep: Seq[String]
+	val website: String
+	val originUrl: String ){
+
+	def presentationPicture() = 
+		if(pictures.isEmpty) 
+			"" 
+		else 
+			pictures.head
+
+
+}
