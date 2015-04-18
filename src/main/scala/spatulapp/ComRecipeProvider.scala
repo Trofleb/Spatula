3342339f9319c30510a4584fc9924b9de4ad1f83
tOverney <tristan.overney@epfl.ch>
2015-04-19 01:22:29 +0200

    fully implemented parsing of recipe.com, left are the link in steps and need to check for < 3 results

--- src/main/scala/spatulapp/ComRecipeProvider.scala
+++ src/main/scala/spatulapp/ComRecipeProvider.scala
@@ -2,11 +2,13 @@ package spatulapp
 
 import scala.util.matching
 import scala.concurrent._
+import org.scalajs.jquery.{JQuery, jQuery}
 
 import scala.concurrent.ExecutionContext.Implicits.global
 
 object ComRecipeProvider extends RecipeProvider {
 
+  val websiteName = "recipe.com"
   val notRecipes = Seq("how-to",
                        "tools",
                        "videos",
@@ -24,25 +26,42 @@ object ComRecipeProvider extends RecipeProvider {
 
   def parseResults(document : String) : Future[Seq[Recipe]] = {
 
-    val recipeLinks = findRecipesLinks(document).take(MAX_RECIPE)
-    val recipes = recipeLinks map {case x : String => IOHandler.get(x)(parseRecipePage)}
-    IOHandler.log(recipes.toList.map(_.toString) mkString(" "))
+    val recipeLinks = findRecipesLinks(document).take(1)
+    val recipes = recipeLinks map {case x : String => IOHandler.get(x)(parseRecipePage(x))}
 
     recipes.foldLeft(Future(Seq.empty[Recipe])){
       case (list , recipe) => for(l <- list; r <- recipe) yield r +: l
     }
   }
 
-  def findRecipesLinks(document : String) : Iterator[String] = {
+  def findRecipesLinks(document : String) : Seq[String] = {
     val pattern = """http\:\/\/www\.recipe\.com\/[a-zA-Z\-]+\/""".r
     val links = pattern findAllIn(document)
-    links filterNot (link => notRecipes.exists(link.contains))
+    (links filterNot (link => notRecipes.exists(link.contains)) toList) distinct
   }
 
-  def parseRecipePage(page : String) : Recipe = {
+  def parseRecipePage(url : String)(page : String) : Recipe = {
     IOHandler.log(page)
-    val patternName = """""".r
-    Recipe("title", 1.0, /*pictures*/ Option("img"), /*ingredients*/ Seq(),
-      /*stepByStep*/ Seq(), "recipe.com", "hello")
+
+    val patternTitle = """<meta property=\"og:title\" content=\"([a-zA-Z\ ]+)\" \/>""".r
+    val title = (patternTitle findAllIn(page)).matchData.next group 1
+
+
+    val patternScore = """<span class=\"hide\" itemprop=\"ratingValue\">(\d(\.\d)*)<\/span>""".r
+    val score = ((patternScore findAllIn(page)).matchData.next group 1).toDouble / 5.0
+
+    val patternPicture = """<img class=\"photo\" itemprop=\"image\" src=\"http\:\/\/www\.recipe\.com\/images\/([a-zA-Z\d\-]+)\.jpg\" """.r
+    val picMatch = ((patternPicture findAllIn(page)).matchData.next) group 1
+    val picture = Some("http://www.recipe.com/images/" + picMatch + ".jpg")
+
+    val patternIngredients = """<div class=\"floatleft ingredient\">\n(\ )*([a-zA-Z\d\ ,\-]+)</div>""".r
+    val ingredients = (((patternIngredients findAllIn(page)).matchData) map ( x => x group 2)) toList
+
+    val withFuckingLinks : Seq[String] = (parseHtml(page).find(".stepbystepInstruction") toArray) map (x => jQuery(x).html)
+    
+    IOHandler.log("hello " + withFuckingLinks.mkString("\n"))
+
+    Recipe(title, score, /*picture*/ picture, /*ingredients*/ ingredients,
+      /*stepByStep*/ withFuckingLinks, websiteName, url)
   }
 }
