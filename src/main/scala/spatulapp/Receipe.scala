d597fe8e2b5ebc8ebc236ac0604d90b7be4e2963
Trofleb <ncasademont@bluewin.ch>
2015-04-18 15:23:27 +0200

    Seq import

--- src/main/scala/spatulapp/Receipe.scala
+++ src/main/scala/spatulapp/Receipe.scala
@@ -1,12 +1,13 @@
+import scala.collection.immutable.Seq
 
 
 class Receipe(
-	val title: String
-	val stars: Int
-	val pictures: Seq[String]
-	val ingredients: Seq[String]
-	val stepByStep: Seq[String]
-	val website: String
+	val title: String,
+	val stars: Int,
+	val pictures: Seq[String],
+	val ingredients: Seq[String],
+	val stepByStep: Seq[String],
+	val website: String,
 	val originUrl: String ){
 
 	def presentationPicture() = 
